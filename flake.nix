b4ddf2309d5d9093e7998898ee903e51d257e413
Nathan Shelly <nsnshelly@gmail.com>
2021-02-06 16:59:22 -0800

    Set up first round of infra

--- flake.nix
+++ flake.nix
@@ -24,44 +24,124 @@
     utils.lib.eachDefaultSystem (
       system:
         let
-          rust-overlay' = import rust-overlay;
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ rust-overlay' ];
+            # apply rust-overlay
+            overlays = [ rust-overlay.overlay ];
           };
 
-          rust = (
-            pkgs.rustChannelOf {
-              date = "2021-01-28";
-              channel = "nightly";
-            }
-          ).rust;
+          # define the rust toolchain used to build `api`
+          #
+          # refs:
+          # - https://github.com/oxalica/rust-overlay#attributes-provided-by-the-overlay
+          # - https://github.com/ebroto/rustup/blob/c2db7dac6b38c99538eec472db9d23d18f918409/README.md#the-toolchain-file
+          rust = pkgs.rust-bin.nightly.latest.rust.override {
+            # the set of additional extensions to install
+            #
+            # TODO: confirm the default set of extensions - https://github.com/oxalica/rust-overlay/issues/14
+            #
+            # refs:
+            # - https://rust-lang.github.io/rustup/concepts/components.html
+            # - https://github.com/oxalica/rust-overlay/blob/e97d64daac9333196691e776d5062713a79bf951/manifest.nix#L10
+            extensions = [
+              # source code for rust standard library - used by rust-analyzer
+              "rust-src"
 
+              # theoretically this is valid but it fails to build with it
+              # ref: https://github.com/oxalica/rust-overlay/blob/e97d64daac9333196691e776d5062713a79bf951/manifest.nix#L21
+              # "rustfmt-preview"
+            ];
+
+            # install standard library for given targets
+            #
+            # refs:
+            # - https://github.com/ebroto/rustup/blob/c2db7dac6b38c99538eec472db9d23d18f918409/README.md#cross-compilation
+            # - https://doc.rust-lang.org/beta/rustc/platform-support.html
+            targets = [ "x86_64-unknown-linux-gnu" "x86_64-apple-darwin" ];
+          };
+
+          # specify the rust and cargo versions for `naersk` to use
           naersk-lib = naersk.lib."${system}".override {
             cargo = rust;
             rustc = rust;
           };
         in
           rec {
-            # `nix build`
-            packages.api = naersk-lib.buildPackage {
-              pname = "api";
-              root = ./.;
-            };
+            # nix <command> . runs the default attribute corresponding to that
+            # command
+            # e.g. `nix run .` runs the `defaultApp`
+
+            defaultApp = apps.api;
             defaultPackage = packages.api;
 
             # `nix run`
             apps.api = utils.lib.mkApp {
               drv = packages.api;
+
+              # `naersk` builds packages whose derivations have names of the
+              # form "{name}-{version}" but whose binaries exclude the version
+              # in the filename. for `mkApp` to find the correct executable
+              # target specify name w/o a version here.
+              name = "api";
             };
-            defaultApp = apps.api;
 
             # `nix develop`
-            devShell = pkgs.mkShell {
-              # supply the specific rust version
-              nativeBuildInputs = with pkgs; [ rust ]
-              ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+            devShell = let
+              cacert = pkgs.cacert;
+            in
+              pkgs.mkShell {
+                # buildInputs = [ pkgs.openssl pkgs.cacert ];
+                # buildInputs = with pkgs; [
+                #   pkgconfig
+                #   openssl
+                #   cmake
+                #   zlib
+                #   libgit2
+                # ];
+
+                # cross-compilation - https://github.com/oxalica/rust-overlay/issues/11#issuecomment-772496493
+
+                nativeBuildInputs = with pkgs; [
+                  # use the rust version we specified above
+                  rust
+                  # can be run manually and also used by `rust-analyzer`
+                  rustfmt
+                  cargo-watch
+                ]
+                # ref - https://stackoverflow.com/a/51161923
+                ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+                # ++ lib.optionals stdenv.isLinux [ pkg-config openssl ];
+
+                shellHook = ''
+                  export SOURCE_CODE="${self.outPath}"
+                '' + (
+                  if pkgs.stdenv.isLinux then ''
+                    # CARGO_HTTP_CAINFO="/nix/store/gdgnc8r39yz1g74bw674flzdw759ml1c-nss-cacert-3.56/etc/ssl/certs/ca-bundle.crt"
+                    export CARGO_HTTP_CAINFO="${cacert}/etc/ssl/certs/ca-bundle.crt"
+                    export SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt"
+                  '' else ""
+                );
+              };
+
+            # `nix build`
+            packages = {
+              docker = pkgs.dockerTools.streamLayeredImage {
+                name = "api";
+                contents = [ self.packages.x86_64-linux.api ];
+                config.Cmd = [ "api" ];
+              };
+
+              # version & name are parsed from Cargo.toml
+              server = naersk-lib.buildPackage {
+                src = ./.;
+
+                # buildInputs = [] ++ lib.optionals stdenv.isLinux [
+                #   pkg-config
+                #   openssl
+                # ];
+              };
             };
+
           }
     );
 }
